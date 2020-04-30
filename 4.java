8609d9d9ef6cab1aa0154ff6dfbf2c0786289b51
zlj1435104045 <1435104045@qq.com>
2020-04-30 16:48:25 +0800

    zlj

---
+++ 4.java
@@ -0,0 +1,24 @@
+package cn.itcast.fruitstore.tools;
+import java.awt.Component;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+/*
+ * 工具类
+ */
+public class GUITools {
+	//JAVA提供的GUI默认工具类对象
+	static Toolkit kit = Toolkit.getDefaultToolkit();
+	//将指定组件屏幕居中
+	public static void center(Component c) {
+		int x = (kit.getScreenSize().width - c.getWidth()) / 2;
+		int y = (kit.getScreenSize().height - c.getHeight()) / 2;
+		c.setLocation(x, y);
+	}
+	//为指定窗口设置图标标题
+	public static void setTitleImage(JFrame frame,String titleIconPath) {
+		frame.setIconImage(kit.createImage(titleIconPath));
+	} 
+}
+————————————————
+版权声明：本文为CSDN博主「搬山」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/weixin_43837863/java/article/details/88804457

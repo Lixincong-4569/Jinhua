5bd2789ab1c1b0bf3acdf598d73ce200e195bff3
Nancyi1113 <267205358@qq.com>
2020-04-30 17:22:32 +0800

    LHR

---
+++ MainFrameController.java
@@ -0,0 +1,25 @@
+package cn.itcast.fruitstore.controller;
+import cn.itcast.fruitstore.view.AbstractMainFrame;
+/**
+ * 主界面操作类
+ */
+@SuppressWarnings("serial")
+public class MainFrameController extends AbstractMainFrame {
+	@Override
+	public void showAdminDialog() {
+		//在该方法中创建管理员界面并显示
+		//this为父窗口(主界面)  true：设置为模态窗口展示
+		new AdminDialogController(this, true).setVisible(true);
+	}
+}
+
+package cn.itcast.fruitstore.app;
+import cn.itcast.fruitstore.controller.MainFrameController;
+/**
+ * 项目运行
+ */
+public class MainApp {
+	public static void main(String[] args) {		
+		new MainFrameController().setVisible(true);
+	}
+}
